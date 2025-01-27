0739fe706f614d08148f8db31ad72696cff13df3
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-01-27 07:55:27 +0530

    runtimes,npm,packages

---
+++ BACKEND/WEEK01/01.mjs
@@ -0,0 +1,4 @@
+import chalk from "chalk";
+console.log(chalk.blue("hello world"));
+//internal package are those provided by node js itself (fs,http,path)
+//external package needs to be installed externally 
---
+++ BACKEND/WEEK01/02.js
@@ -0,0 +1,4 @@
+const path = require("path");
+console.log(__dirname);
+console.log(__dirname + "/../../index.js" + "/projects");
+console.log(path.join("C:\Users\aloom\Downloads\LEARNING-WEBDEV\BACKEND\WEEK01","../../index.js","/projects"))
